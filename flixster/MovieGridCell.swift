0d2bd93c0150f5de195dfb46ef577e4c19b239d1
David Neuman <davidsneuman@gmail.com>
2021-02-15 13:29:49 -0500

    Part 2 of flixster

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by David Neuman on 2/15/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
